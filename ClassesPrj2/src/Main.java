acecba26b93c645f1eed34fdd4cd23e51b503e23
jirwincline <jci@pdx.edu>
2020-05-24 09:03:55 -0700

    finish up class portion

--- ClassesPrj2/src/Main.java
+++ ClassesPrj2/src/Main.java
@@ -1,9 +1,40 @@
 
+class Account {
+	private double balance = 0;
+	
+	public void debit(double amount) {
+		if (amount > 0 && amount <= balance) {
+			balance -= amount;
+		}
+	}
+	
+	public void credit(double amount) {
+		if (amount > 0) {
+			balance += amount;
+		}
+	}
+	
+	public void showBalance() {
+		System.out.println("Current balance - " + balance);
+	}
+	
+}
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Account al;
+		
+		al = new Account();
+		
+		al.credit(2000);
+		al.debit(1000);
+		
+		al.showBalance();
+		
+		al.debit(-2000);
+		al.showBalance();
 	}
 
 }
---
+++ ClassesPrj3/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+class ShoppingCart {
+	
+	private LinkedList products = new LinkedList();
+	
+	public void addToCart(String product) {
+		products.add(product);
+	}
+	
+	public void order() {
+		System.out.println("Ordering Products");
+		System.out.println(products);
+	}
+}
+public class Main {
+
+	public static void main(String[] args) {
+		ShoppingCart cart1 = new ShoppingCart();
+		cart1.addToCart("a");
+		cart1.addToCart("b");
+		cart1.addToCart("c");
+		
+		cart1.order();
+
+	}
+
+}
