8162b5ba3a4f4499c22f289b621fb20ba997512f
jirwincline <jci@pdx.edu>
2020-05-24 10:50:05 -0700

    add date format class

--- StaticPrj1/src/Main1.java
+++ StaticPrj1/src/Main1.java
@@ -1,9 +1,22 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+class MyDateUtil {
+	final static String TARGET_DATE_PATTERN = "mm/dd/yyyy";
+	public static String getFormattedDate(Date d) {
+		SimpleDateFormat sdf = new SimpleDateFormat(TARGET_DATE_PATTERN);
+		return sdf.format(d);
+	}
+}
 public class Main1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MyDateUtil util = new MyDateUtil();
+		
+		Date now = new Date();
+		System.out.println( now );
+		String formattedDate = MyDateUtil.getFormattedDate(now);
+		System.out.println( formattedDate );
 	}
 
 }
