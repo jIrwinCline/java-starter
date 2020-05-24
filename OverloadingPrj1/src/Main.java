f2fabe9cccd792e60fc22975923208f622038ab9
jirwincline <jci@pdx.edu>
2020-05-24 09:54:00 -0700

    implement overloading forms of print function

---
+++ OverloadingPrj1/src/Main.java
@@ -0,0 +1,24 @@
+
+class Sample {
+	public void print(int a) {
+		System.out.println("int - " + a);
+		
+	}
+	public void print(double a) {
+		System.out.println("double - " + a);
+	}
+	public void print(String a) {
+		System.out.println("string - " + a);
+	}
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+		Sample obj = new Sample();
+		obj.print(10);
+		obj.print(10.00);
+		obj.print("10");
+	}
+
+}
