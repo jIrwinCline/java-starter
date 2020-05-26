a4f0df612da45ecc4ecf04ae72b9c90ee0ad5829
jirwincline <jci@pdx.edu>
2020-05-26 15:25:16 -0700

    Implement final keyword in classes

---
+++ FinalModifierPrj1/src/Main.java
@@ -0,0 +1,26 @@
+// wouldnt be able to be modified
+//final class Base {
+//	public final void f() {
+//		
+//	}
+//}
+class Base {
+	public final void f() {
+		
+	}
+}
+class Derived extends Base {
+	// cannot overide
+//	@Override
+//	public void f() {
+//		
+//	}
+}
+public class Main {
+public static final double PI = 3.14159;
+	public static void main(String[] args) {
+		double x = Main.PI;
+		
+	}
+
+}
