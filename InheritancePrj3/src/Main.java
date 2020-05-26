838a1da15e236c0b0054c735d1fb9d1a3fa340a8
jirwincline <jci@pdx.edu>
2020-05-26 13:21:32 -0700

    tested super and constructors with inheritance classes

---
+++ InheritancePrj3/src/Main.java
@@ -0,0 +1,25 @@
+class Base {
+	public void print() {
+		System.out.print(" Hello");
+	}
+}
+class Derived extends Base {
+	@Override
+	public void print() {
+		super.print();
+		System.out.print(" World");
+	}
+}
+public class Main {
+
+	public static void main(String[] args) {
+		Base b1 = new Base();
+		b1.print(); // Hello
+		
+		System.out.println();
+		
+		Derived d1 = new Derived();
+		d1.print(); //World
+	}
+
+}
